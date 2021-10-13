9669e44c780987ea18a1fe3b51617923d852e7bb
CarJou <carlajoumier@gmail.com>
2021-10-13 13:43:25 -0300

    added movie card

--- src/components/PopularMovie/index.jsx
+++ src/components/PopularMovie/index.jsx
@@ -1,8 +1,9 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 
-import play from '../../assets/icons/play.png';
 import star from '../../assets/icons/star.png';
+import Zoom from 'react-reveal/Zoom'; // Importing Zoom effect
+import Fade from 'react-reveal/Fade';
 
 const PopularMovie = ({ fetchUrl, imgUrl }) => {
   const [popularMovie, setPopularMovie] = useState([]);
@@ -24,27 +25,41 @@ const PopularMovie = ({ fetchUrl, imgUrl }) => {
     <div className="card-container">
       {popularMovie &&
         popularMovie.map((movie) => (
-          <div className="card-content" key={movie.id}>
-            <img
-              src={`${imgUrl}${movie.backdrop_path}`}
-              alt={movie.title}
-              onClick={() => handleClick(movie)}
-              key={movie.id}
-              className="img-card"
-            />{' '}
-            <div className="card-info">
-              <button className="button-card"></button>
+          <Zoom>
+            <div className="card-content" key={movie.id}>
+              <img
+                src={`${imgUrl}${movie.backdrop_path}`}
+                alt={movie.title}
+                onClick={() => handleClick(movie)}
+                key={movie.id}
+                className="img-card"
+              />{' '}
+              <div className="card-content-info">
+                <Fade left>
+                  <div className="card-header-info">
+                    <button className="button-card"></button>
 
-              <h1 className="card-title">{movie.title}</h1>
-              <h1 className="card-star">
-                <img src={star} alt={movie.vote_average} />
-                {movie.vote_average}
-              </h1>
-              <h1 className="card-date">
-                {movie.release_date.substring(0, 4)}
-              </h1>
+                    <h1 className="card-title">{movie.title}</h1>
+                  </div>
+                </Fade>
+                <Fade right>
+                  <div className="card-info">
+                    <h1 className="card-star">
+                      <img
+                        src={star}
+                        alt={movie.vote_average}
+                        className="svg-star"
+                      />
+                      {movie.vote_average}
+                    </h1>
+                    <h1 className="card-date">
+                      {movie.release_date.substring(0, 4)}
+                    </h1>
+                  </div>
+                </Fade>
+              </div>
             </div>
-          </div>
+          </Zoom>
         ))}
     </div>
   );
